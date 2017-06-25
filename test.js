6297f889104e11d773c7aeb74fef4a713ede8d9e
Daan Sprenkels <hello@dsprenkels.com>
2017-06-25 02:10:01 +0200

    Implement property testing

--- test.js
+++ test.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
 
 const sss = require("./lib");
+const jsc = require("jsverify");
+const shuffle = require('shuffle-array');
 
 const data = Buffer.alloc(64, 0x42);
-const n = 200;
-const k = 100;
 
-let p = sss.createShares(data, n, k)
-    .then((x) => {
-        return sss.combineShares(x);
-    })
-    .then((x) => {
-        console.log(x);
-        process.exit();
+
+let normalOperation = jsc.forall(jsc.integer(1, 255), jsc.integer(1, 255),
+                                 jsc.integer(1, 255), (n, k, k2) => {
+    // We can't really test anything if not n is not larger than k and k2
+    if (k > n || k2 > n) return true;
+    let sharesP = sss.createShares(data, n, k);
+    let restoredP = sharesP.then(shares => {
+        let newShares = shuffle(shares).slice(0, k2);
+        return sss.combineShares(newShares);
     });
+    return restoredP.then((restored) => {
+      return k2 >= k;
+    }).catch((error) => {
+      return k2 < k;
+    })
+  }
+);
+
+jsc.assert(normalOperation);
