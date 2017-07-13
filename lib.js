842165d64c11854579506ac283e6b2684cdebab5
Daan Sprenkels <hello@dsprenkels.com>
2017-07-14 00:35:44 +0800

    Remove duplicates before combining shares

--- lib.js
+++ lib.js
@@ -4,6 +4,21 @@ const crypto = require("crypto");
 const _sss = require("./build/Release/shamirsecretsharing");
 
 
+function removeDuplicates(shares) {
+  if (!shares) return shares;
+  shares.sort();
+  let i = 1;
+  while (i < shares.length) {
+    if (shares[i].equals(shares[i-1])) {
+      shares.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return shares;
+}
+
+
 exports.createShares = function createShares(data, n, k) {
   return new Promise((resolve) => {
     _sss.createShares(data, n, k, resolve);
@@ -13,7 +28,7 @@ exports.createShares = function createShares(data, n, k) {
 
 exports.combineShares = function combineShares(shares) {
   return new Promise((resolve) => {
-    _sss.combineShares(shares, resolve);
+    _sss.combineShares(removeDuplicates(shares), resolve);
   }).then((x) => {
     if (x === null) {
       throw "InvalidAccessError: invalid or too few shares provided";
--- test.js
+++ test.js
@@ -10,35 +10,45 @@ const key = data.slice(32);
 
 let standardOperation = jsc.forall(jsc.integer(1, 255), jsc.integer(1, 255),
                                    jsc.integer(1, 255), (n, k, k2) => {
-    // We can't really test anything if not n is not larger than k and k2
-    if (k > n || k2 > n) return true;
-    let sharesP = sss.createShares(data, n, k);
-    let restoredP = sharesP.then(shares => {
-      let newShares = shuffle(shares).slice(0, k2);
-      return sss.combineShares(newShares);
-    });
-    return restoredP.then((restored) => {
-      return (k2 >= k) && restored.equals(data);
-    }).catch((error) => {
-      return k2 < k;
-    });
-  }
-);
+  // We can't really test anything if not n is not larger than k and k2
+  if (k > n || k2 > n) return true;
+  let sharesP = sss.createShares(data, n, k);
+  let restoredP = sharesP.then(shares => {
+    let newShares = shuffle(shares).slice(0, k2);
+    return sss.combineShares(newShares);
+  });
+  return restoredP.then((restored) => {
+    return (k2 >= k) && restored.equals(data);
+  }).catch((error) => {
+    return k2 < k;
+  });
+});
+
+let doubleShareTest = jsc.forall(jsc.integer(1, 255), (n) => {
+  let sharesP = sss.createShares(data, n, 1);
+  let restoredP = sharesP.then(shares => {
+    let newShare = shuffle(shares)[0];
+    return sss.combineShares([newShare, newShare]);
+  });
+  return restoredP.then((restored) => {
+    return restored.equals(data);
+  });
+});
 
 let hazmatOperation = jsc.forall(jsc.integer(1, 255), jsc.integer(1, 255),
                                  jsc.integer(1, 255), (n, k, k2) => {
-    // Unable to test if `k` or `k2` is larger than `n`
-    if (k > n || k2 > n) return true;
-    let keysharesP = sss.createKeyshares(key, n, k);
-    let restoredP = keysharesP.then((keyshares) => {
-      let newKeyshares = shuffle(keyshares).slice(0, k2);
-      return sss.combineKeyshares(newKeyshares);
-    });
-    return restoredP.then((restored) => {
-      return (k2 >= k) == restored.equals(key);
-    });
-  }
-);
+  // Unable to test if `k` or `k2` is larger than `n`
+  if (k > n || k2 > n) return true;
+  let keysharesP = sss.createKeyshares(key, n, k);
+  let restoredP = keysharesP.then((keyshares) => {
+    let newKeyshares = shuffle(keyshares).slice(0, k2);
+    return sss.combineKeyshares(newKeyshares);
+  });
+  return restoredP.then((restored) => {
+    return (k2 >= k) == restored.equals(key);
+  });
+});
 
 jsc.check(standardOperation, { tests: 1000 });
+jsc.check(doubleShareTest, { tests: 100 });
 jsc.check(hazmatOperation, { tests: 1000 });
